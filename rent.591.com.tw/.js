dd3d79664d55763031cf25347ba1ddc75a855e24
ComebuyCB <ben2000200066@yahoo.com.tw>
2022-01-19 18:51:00 +0800

    591 sortTable & autoUpdateData

--- rent.591.com.tw/.js
+++ rent.591.com.tw/.js
@@ -1,33 +1,47 @@
 $(function(){
-	setTimeout(function() {
+	if ( window.location.href.indexOf('rent.591.com.tw/home') == -1 ){
 		$('body').append(`
 			<div id="CBfixed">
-				<button id="CBbtn" onclick="cb_datchData()" js-popTarget="#CBdiv">TB</button>
+				<div style="display: flex">
+					<button class="CBbtn" onclick="$('#CBdiv').toggle()">TB</button>
+					<button class="CBbtn js-prev" onclick="$('.pagePrev').not('.last').children().click();">←</button>
+					<button class="CBbtn js-next" onclick="$('.pageNext').not('.last').children().click();">→</button>
+				</div>
 				<div id="CBdiv" style="display: none"></div>
 			</div>
 		`)
-		new PopupTargets
-	});
+		cb_afterRender()
+	}
 })
 
-$(document).one('click', '#CBbtn', function(){
-	cb_datchData()
-	vm.$watch('listData', ()=>{
-		cb_datchData()
-	})
-})
 
-function cb_datchData(){
-	// $('#CBdiv').toggle()
+function cb_afterRender( To ){
+	let t1 = setTimeout(()=>{
+		if ( $('#j-loading').css('display') === 'none' ){
+			cb_getData()
+			vm.$watch('listData', ()=>{
+				cb_getData()
+			})
+		} else {
+			cb_afterRender()
+		}
+		clearTimeout( t1 )
+	}, 100 )
+}
+
+
+function cb_getData(){
 	console.log(vm._data.listData)
 	let tr = ""
 	$.each( vm._data.listData, function(idx,ele){
-		let td  = `<td><a target="_blank" href="https://rent.591.com.tw/home/${ele.post_id}">${ele.title}</a></td>`
-			td += `<td>${ele.location}</td>`
-			td += `<td>${ele.floor_str}</td>`
-			td += `<td>${ele.surrounding.desc = ele.surrounding.type === "subway_station" ? ele.surrounding.desc : "" }</td>`
-			td += `<td>${ele.surrounding.distance = ele.surrounding.type === "subway_station" ? ele.surrounding.distance.replace('公尺','') : "" }</td>`
-			td += `<td>${ele.price}</td>`
+		let td  = `
+			<td><a target="_blank" href="https://rent.591.com.tw/home/${ele.post_id}">${ele.title}</a></td>
+			<td>${ele.location}</td>
+			<td>${ele.floor_str}</td>
+			<td>${ele.surrounding.desc = ele.surrounding.type === "subway_station" ? ele.surrounding.desc : "" }</td>
+			<td>${ele.surrounding.distance = ele.surrounding.type === "subway_station" ? ele.surrounding.distance.replace('公尺','') : "" }</td>
+			<td>${+ele.price.replace(',','')}</td>
+		`
 		tr += `<tr>${td}</tr>`
 	})
 	
@@ -35,12 +49,12 @@ function cb_datchData(){
 		<table id="CBtable">
 			<thead>
 				<tr>
-					<th onclick="sortTable(0)">標題</th>
-					<th onclick="sortTable(1)">地點</th>
-					<th onclick="sortTable(2)">樓層</th>
-					<th onclick="sortTable(3)">捷運</th>
-					<th onclick="sortTable(4)">距離</th>
-					<th onclick="sortTable(5)">價格</th>
+					<th>標題</th>
+					<th>地點</th>
+					<th>樓層</th>
+					<th>捷運</th>
+					<th>距離</th>
+					<th>價格</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -48,100 +62,36 @@ function cb_datchData(){
 			</tbody>
 		</table>
 	`)
-}
-
-
-function sortTable(n) {
-	let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
-	table = document.getElementById("CBtable")
-	switching = true
-	dir = "asc"
-	while (switching) {
-	    switching = false
-	    rows = table.rows
-	    for (i = 1; i < (rows.length - 1); i++) {
-			shouldSwitch = false
-			x = rows[i].getElementsByTagName("TD")[n]
-			y = rows[i + 1].getElementsByTagName("TD")[n]
-			if (dir == "asc") {
-				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-			    	shouldSwitch= true
-			    	break
-				}
-			} else if (dir == "desc") {
-				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-					shouldSwitch = true
-					break
-				}
-			}
-		}
-		if (shouldSwitch) {
-			rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
-			switching = true
-			switchcount ++   
-		} else {
-			if (switchcount == 0 && dir == "asc") {
-				dir = "desc"
-				switching = true
-			}
-		}
-		
+	
+	sortTable()
+	
+	if ( $('.pagePrev').hasClass('first') ){
+		$('.CBbtn.js-prev').hide()
+	}else{
+		$('.CBbtn.js-prev').show()
+	}
+	
+	if ( $('.pageNext').hasClass('last') ){
+		$('.CBbtn.js-next').hide()
+	}else{
+		$('.CBbtn.js-next').show()
 	}
 }
 
 
-class PopupTargets {
-    constructor(){
-        this.targets = []
-        if ( $('[js-popTarget]').length > 0 ){
-            this.showTarget()
-            this.filterTarget()
-            this.hideTarget()
-        }
-    }
-
-    showTarget(){
-        // 設置 js-popTarget 的目標，click 後打開目標
-        let _this = this
-        $('[js-popTarget]').each( function(){
-            let target = $(this).attr('js-popTarget')
-            _this.targets.push( target )
-            $(this).on('click',function(){
-                if ( $(this).attr('js-popTargetMode') === 'toggle' ){
-                    $(target).toggle()
-                } else {
-                    $(target).show()
-                }
-            })
-        })
-    }
-
-    // 篩選重複
-    filterTarget(){
-        this.targets.filter( (elem, index, arr)=>{
-            return arr.indexOf(elem) === index
-        })
-    }
-
-    // 按下其他地方時，隱藏目標
-    hideTarget(){
-        $.each( this.targets, function( index, elem ){
-            $(document).click(function(e) {
-                let targetName = $(e.target).attr('js-popTarget') ? 
-                    $(e.target).attr('js-popTarget') : 
-                    $(e.target).closest('[js-popTarget]').attr('js-popTarget')
-
-                /* 隱藏目標條件：
-                    1. [按下處html] 是 [目標html] 
-                    2. [按下處html] 是 [目標html] 的裡面
-                    3. [按下處是開關，開關的目標名字] 等於 [目標名字]
-                    4. [按下處的父類是開關，開關的目標名字] 等於 [目標名字]
-                    以上皆非，則關閉目標。
-                */
-                if ( !$(elem).is(e.target) && $(elem).has(e.target).length === 0 && !$(elem).is( $(targetName) ) ) {
-                    $(elem).hide()
-                }
-            })
-        })
-    }
+function sortTable(){
+	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+	
+	const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+	    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+	    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+	
+	// do the work...
+	document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+	  const table = th.closest('table');
+	  const tbody = table.querySelector('tbody');
+	  Array.from(tbody.querySelectorAll('tr'))
+	    .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+	    .forEach(tr => tbody.appendChild(tr) );
+	})))
 }
