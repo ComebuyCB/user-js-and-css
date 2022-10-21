a60f936706f63c900139fbc09d5021f498d9139f
ComebuyCB <ben2000200066@yahoo.com.tw>
2022-10-21 21:08:00 +0800

    update~

--- www.foodpanda.com.tw/.js
+++ www.foodpanda.com.tw/.js
@@ -1,5 +1,5 @@
 $(function(){
-	$(document).on('click','.photo',function(){
+	$(document).on('click','.lazy-loaded-dish-photo', function(){
 		let urlImg = $(this).css('background-image')
 		let imgSm, imgLg
 		if (urlImg){
@@ -15,55 +15,29 @@ $(function(){
 	$(document).on('click','._photoShow',function(){
 		$(this).remove()
 	})
-
-	$('body').append(`
-		<div class="CB_fixed">
-			<button class="CB_btn" onclick="photoMode()">切換大圖</button>
-			<div class="CB_div" style="display: none"></div>
-		</div>
-	`)
-})
-
-function photoMode(){
-	$('.CB_div').toggle()
-	$('body').toggleClass('bodyNowrap')
 	
-	let section = ''
-	$.each( $('.menu__items-wrapper .dish-category-section'), function(idx,item){
-		let title = $(item).find('.dish-category-title').text()
-		section += `<h3 class="title">${title}</h3>`
-		
-		let cont = ''
-		$.each( $(item).find('.dish-card'), function(){
-			let name = $(this).find('[data-testid=menu-product-name]').text()
-			let price = $(this).find('[data-testid=menu-product-price]').text()
-			let photo
-			
-			let urlImg = $(this).find('.photo').css('background-image')
-			let picture = $(this).find('picture').length > 0
-			if (urlImg){
-				photo = urlImg.replace( /url\("(.+)"\)/g,'$1').replace( /width=(\d)+/g,'width=600')
-			} else if (picture){
-				photo = "https://micro-assets.foodora.com/img/logo-simple-fp.svg"
-			} else {
-				
-			}
-			
-			if ( price == "$ 0" ){
-				
-			}else{
-				cont += `
-					<div class="bgImg" style="background-image: url(${photo})">
-						<div class="bgTxt">${name} (${price})</div>
-					</div>
-				`
+	let CB_detectCart = setInterval(()=>{
+		if ( $('.cart-summary-footer').length ){
+			$('.cart-summary-footer').prepend('<button id="CB_copyCart">copy</button>')
+			clearInterval( CB_detectCart )
+		}
+	}, 100 )
+
+	$(document).on('click', '#CB_copyCart', function(){
+		let str = ''
+		let $cartLists = $('.cart-summary-item-list').children()
+		$cartLists.each( function(idx, ele){
+			let title = $(ele).find('.product-card-name').text()
+			let price = $(ele).find('.product-card-price').text()
+			let added = ''
+			$(ele).find('.cl-interaction-secondary').each( function( sIdx, sEle ){
+				added += $(sEle).text() + '\n'
+			})
+			str += title + '\n' + added + 'total: ' + price + '\n'
+			if ( idx !== $cartLists.length - 1){ 
+				str += '---\n'
 			}
 		})
-		section += `<div style="display:flex; flex-wrap: wrap">${cont}</div><hr>`
-		
+		navigator.clipboard.writeText(str)
 	})
-	
-	$('.CB_div').html(`
-		${section}
-	`)
-}
+})
